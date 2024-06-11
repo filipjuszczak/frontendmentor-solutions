5ec9d314993c0e6ce6b7959a4613ffbbc3b45a80
Filip Juszczak <juszczak.filip24@gmail.com>
2024-06-11 18:56:44 +0200

    increase size of Step component

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -6,7 +6,7 @@ interface StepProps {
 export default function Step({ children, className }: StepProps) {
   return (
     <div
-      className={`text-slate border-slate/90 before:bg-slate/90 relative mx-auto flex size-12 items-center justify-center rounded-full border font-bold before:absolute before:left-1/2 before:top-0 before:h-24 before:w-[1px] before:origin-top before:rotate-180 before:content-[''] ${className ? className : ""}`}
+      className={`relative mx-auto flex size-16 items-center justify-center rounded-full border border-slate/90 font-bold text-slate before:absolute before:left-1/2 before:top-0 before:h-24 before:w-[1px] before:origin-top before:rotate-180 before:bg-slate/90 before:content-[''] ${className ? className : ""}`}
     >
       {children}
     </div>
