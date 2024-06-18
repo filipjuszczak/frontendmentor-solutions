7e7abb92ee12a8fed8c0a8196c2474459033c833
Filip Juszczak <juszczak.filip24@gmail.com>
2024-06-18 17:47:00 +0200

    re-write news-homepage solution
    add responsivity
    improve accessibility
    optimize font

--- html/news-homepage/script.js
+++ html/news-homepage/script.js
@@ -1,17 +1,22 @@
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const nav = document.querySelector('#primary-navigation');
+"use strict";
+
+const navToggle = document.querySelector("#mobile-navigation-button");
+const nav = document.querySelector("#primary-navigation");
+const overlay = document.querySelector(".overlay");
 const html = document.documentElement;
 
-navToggle.addEventListener('click', () => {
-  const isExpanded = nav.getAttribute('aria-expanded');
+navToggle.addEventListener("click", () => {
+  const isExpanded = nav.getAttribute("aria-expanded");
 
-  if (isExpanded === 'true') {
-    nav.setAttribute('aria-expanded', 'false');
-    navToggle.setAttribute('data-expanded', 'false');
-    html.classList.remove('scroll-lock');
+  if (isExpanded === "true") {
+    nav.setAttribute("aria-expanded", "false");
+    navToggle.setAttribute("data-expanded", "false");
+    overlay.classList.remove("overlay-visible");
+    html.classList.remove("scroll-lock");
   } else {
-    nav.setAttribute('aria-expanded', 'true');
-    navToggle.setAttribute('data-expanded', 'true');
-    html.classList.add('scroll-lock');
+    nav.setAttribute("aria-expanded", "true");
+    navToggle.setAttribute("data-expanded", "true");
+    overlay.classList.add("overlay-visible");
+    html.classList.add("scroll-lock");
   }
 });
