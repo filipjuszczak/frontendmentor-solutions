00ed3798e06d88eac39167e970a49f8c8d2f0aa7
Filip Juszczak <juszczak.filip24@gmail.com>
2024-06-19 15:18:51 +0200

    add width and height attributes to avatar img

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,6 +4,12 @@ interface AvatarProps {
 
 export default function Avatar({ src }: AvatarProps) {
   return (
-    <img src={src} alt="" className="mb-4 rounded-full border-4 border-white" />
+    <img
+      src={src}
+      alt=""
+      className="mb-4 rounded-full border-4 border-white"
+      width={96}
+      height={96}
+    />
   );
 }
