5191e75135ffddb9a03aa09502eff0942b8d7950
Filip Juszczak <juszczak.filip24@gmail.com>
2024-06-21 21:07:36 +0200

    fix typing in context

--- src/context/CartContext.ts
+++ src/context/CartContext.ts
@@ -3,12 +3,19 @@ import { Product } from "../App";
 
 export const CartContext = createContext<{
   cart: Product[];
-  addItem: (item) => void;
-  removeItem: (id) => void;
+  addItem: (item: {
+    id: string;
+    name: string;
+    price: number;
+    currency: string;
+    quantity: number;
+    image: string;
+  }) => void;
+  removeItem: (id: string) => void;
   clearCart: () => void;
 }>({
   cart: [],
-  addItem: (item: any) => {},
-  removeItem: (id: string) => {},
+  addItem: () => {},
+  removeItem: () => {},
   clearCart: () => {}
 });
